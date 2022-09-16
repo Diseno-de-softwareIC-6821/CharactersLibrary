5b44256f14d9f589cf2966e3df52ab8f990a255c
Corne1401 <47864642+danielcornejo14@users.noreply.github.com>
2022-09-15 23:32:58 -0600

    Frecuent cases examples

---
+++ src/examples.ts
@@ -0,0 +1,66 @@
+//constant: can't change in the future
+const a: number = 1;
+
+//variable: can change in the future
+let b: any = 2;
+
+// How to declare a function
+function add(a: number, b: any) {
+    return a + b;
+}
+// How to declare a function with types
+function add2(a: number, b: number): number {
+    return a + b;
+}
+
+// How to declare a function with types and default value
+function add3(a: number, b: number = 2): number {
+    return a + b;
+}
+
+// How to declare an interface
+interface PersonInterface {
+    name: string;
+    age: number;
+    greet(): string;
+}
+
+// How to declare a class
+class Person implements PersonInterface {
+
+    name: string;
+    age: number;
+    constructor(name: string, age: number) {
+        this.name = name;
+        this.age = age;
+    }
+
+    // How to declare a method
+    greet() {
+        return `Hello, my name is ${this.name} and I am ${this.age} years old`;
+    }
+}
+
+// How to declare an object
+const person: Person = new Person("John", 30);
+
+// How to declare an array
+const numbers: number[] = [1, 2, 3, 4, 5];
+
+// How to declare a tuple
+const tuple: [string, number] = ["John", 30];
+
+// How to loop through an array
+for (let i = 0; i < numbers.length; i++) {
+    console.log(numbers[i]);
+}
+
+// How to loop through an array with for...of
+for (let number of numbers) {
+    console.log(number);
+}
+
+// use map to loop through an array
+numbers.map((number) => {
+    console.log(number);
+});
